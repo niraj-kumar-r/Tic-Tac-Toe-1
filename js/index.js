95f4eca804ec9cc18a1098027653ac86181dff46
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-10 23:11:23 +0530

    added the boardArray

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const board = document.getElementsByClassName("board")[0];
 let playerTurn = 1;
 
+const boardArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
 board.addEventListener("click", function updateBoardState(event) {
     event.target.classList.add(`board-piece-active-${playerTurn}`);
     playerTurn = playerTurn === 1 ? 2 : 1;
