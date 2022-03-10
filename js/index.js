974b9d6d82bae08590a3a38e171c203b48652fa3
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-10 22:32:12 +0530

    added active state 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 const board = document.getElementsByClassName("board")[0];
 console.log(board);
+
+board.addEventListener("click", (event) => {
+    console.log(event.target.id);
+    event.target.classList.add("board-piece-active-1");
+});
