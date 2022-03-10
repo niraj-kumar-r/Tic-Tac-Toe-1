d2e1aafacda1edbfc850b5af065ad25993a213f7
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-10 23:19:58 +0530

    last for today

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 const board = document.getElementsByClassName("board")[0];
 let playerTurn = 1;
 
-const boardArray = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-];
-
 board.addEventListener("click", function updateBoardState(event) {
     event.target.classList.add(`board-piece-active-${playerTurn}`);
     playerTurn = playerTurn === 1 ? 2 : 1;
 });
+
+function play() {
+    const boardArray = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    const piecesAvailable = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
