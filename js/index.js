2175a6db4396e30708a43888bed658e2947a2977
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-11 22:14:04 +0530

    added winCheck and win condition

--- js/index.js
+++ js/index.js
@@ -20,12 +20,53 @@ function updateBoard(piece) {
         boardArray[Number(piece.id[0]) - 1][Number(piece.id[1]) - 1] =
             playerTurn === 1 ? 1 : -1;
 
-        if (winCheck(boardArray)) {
-        }
         piece.classList.add(`board-piece-active-${playerTurn}`);
+
+        let winState = winCheck(boardArray);
+
+        if (winState === 1) {
+            document.querySelector("bottom-text").textContent = "Player 1 won";
+        } else if (winState === -1) {
+            document.querySelector("bottom-text").textContent = "Player 2 won";
+        }
 
         playerTurn = playerTurn === 1 ? 2 : 1;
     } else if (!piecesAvailable.includes(piece.id)) {
         console.log("Place Occupied");
+    }
+}
+
+function winCheck(array, side = 3) {
+    const sumArray = [];
+    let dia1 = 0;
+    let dia2 = 0;
+
+    for (let i = 0; i < side; i++) {
+        let rowSum = 0;
+        let colSum = 0;
+
+        for (let j = 0; j < side; j++) {
+            rowSum += array[i][j];
+            colSum += array[j][i];
+        }
+
+        dia1 += array[i][i];
+        dia2 = +array[j][side - i - 1];
+
+        sumArray.push(rowSum);
+        sumArray.push(colSum);
+    }
+
+    sumArray.push(dia1);
+    sumArray.push(dia2);
+
+    // row1 ,col1,row2,col2,row3,col3,dia1,dia2
+
+    if (sumArray.includes(side)) {
+        return 1;
+    } else if (sumArray.includes(-side)) {
+        return -1;
+    } else {
+        return 0;
     }
 }
