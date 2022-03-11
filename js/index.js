6638fb87efaca49d40d13daa507018acccdc21ae
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-11 21:46:10 +0530

    made selecting the same piece not possible

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
-const board = document.getElementsByClassName("board")[0];
 let playerTurn = 1;
 
-board.addEventListener("click", function updateBoardState(event) {
-    event.target.classList.add(`board-piece-active-${playerTurn}`);
-    playerTurn = playerTurn === 1 ? 2 : 1;
+let boardArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+let piecesAvailable = ["11", "12", "13", "21", "22", "23", "31", "32", "33"];
+
+const board = document.getElementsByClassName("board")[0];
+
+board.addEventListener("click", function play(event) {
+    let avail = updateBoard(event.target);
 });
 
-function play() {
-    const boardArray = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
-    const piecesAvailable = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function updateBoard(piece) {
+    if (piecesAvailable.includes(piece.id)) {
+        piecesAvailable = piecesAvailable.filter((a) => a !== piece.id);
+        console.log(piecesAvailable);
+
+        piece.classList.add(`board-piece-active-${playerTurn}`);
+        playerTurn = playerTurn === 1 ? 2 : 1;
+    } else if (!piecesAvailable.includes(piece.id)) {
+        console.log("Place Occupied");
+    }
 }
